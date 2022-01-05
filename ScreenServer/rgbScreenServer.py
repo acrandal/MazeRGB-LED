4978e7e65619503168b9cf9c0d9da5f3149dbc7e
Aaron S. Crandall <crandall@gonzaga.edu>
2022-01-04 17:15:29 -0800

    RabbitMQ RGB Server Server

---
+++ ScreenServer/rgbScreenServer.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+from samplebase import SampleBase
+
+from random import randint, uniform
+from time import sleep
+
+import pika, os, sys
+import json
+from pprint import pprint
+
+
+class Coordinate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Color:
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+
+class Pixel:
+    def __init__(self, coordinate, color):
+        self.coordinate = coordinate
+        self.color = color
+
+
+class ScreenServer(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(ScreenServer, self).__init__(*args, **kwargs)
+
+    def drawPixel(self, pixel):
+        self.matrix.SetPixel(pixel.coordinate.x, pixel.coordinate.y, pixel.color.r, pixel.color.g, pixel.color.b)
+
+
+    def jsonHandler(self, msg):
+        try:
+            dat = json.loads(msg)
+        except:
+            print(f"JSON parse fail: {msg}")
+            return
+        #pprint(dat)
+
+        if dat["type"] == "clear":
+            self.matrix.Clear()
+        elif dat["type"] == "drawPixel":
+            coord = Coordinate(dat["pixel"]["coordinate"]["x"],
+                               dat["pixel"]["coordinate"]["y"])
+            color = Color(
+                dat["pixel"]["color"]["r"],
+                dat["pixel"]["color"]["g"],
+                dat["pixel"]["color"]["b"])
+
+            pixel = Pixel(coord, color)
+
+            self.drawPixel(pixel)
+
+
+    def messageHandler(self, ch, method, properties, body):
+        msg = str(body, 'utf-8')
+        if msg[0] == '{':
+            self.jsonHandler(msg)
+        else:
+            print(f"Unknown message format: {msg}")
+
+
+    def run(self):
+        queueName = 'MazeScreen'
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+
+        channel.basic_consume(queue=queueName, on_message_callback=self.messageHandler, auto_ack=True)
+
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        channel.start_consuming()
+
+
+# Main function
+if __name__ == "__main__":
+    print("Starting RGB server")
+    screenServer = ScreenServer()
+    if (not screenServer.process()):
+        screenServer.print_help()
+
+    print("Shutting down.")
